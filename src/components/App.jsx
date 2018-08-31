c26a4dd7ddfc800f2f5595ed00654c804ae35c88
matthew quinn <matthewquinn.personal@gmail.com>
2018-08-31 11:09:15 -0400

    Created Links to navigate boilerplate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { Menu } from './Menu.jsx'
 import { Home } from './Home.jsx'
 import { About } from './About.jsx'
 
 export const App = () => (
   <Router>
     <div>
+  <Menu />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Menu = () => (
+  <div>
+    <Link to="/"> Home </Link>
+    <Link to="/about"> About </Link>
+  </div>
+)
